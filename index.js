558d695de3492d175cc841b227529bed935b0e88
saji2000 <sajaddaneshmand79@gmail.com>
2025-02-15 14:15:19 -0500

    Created the node app, praise God

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello");
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from the server" });
+});
+
+app.listen(process.env.PORT, (req, res) => {
+  console.log("listen on port: ", process.env.PORT);
+});
