6de36fd04539bfa31ec78e1090710aa844c523de
saji2000 <sajaddaneshmand79@gmail.com>
2025-02-16 15:23:00 -0500

    cors, helmet and cookie parser added, praise God

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const cookieParse = require("cookie-parser");
 
 const app = express();
-
+app.use(cors());
+app.use(helmet());
 app.use(express.json());
+app.use(cookieParse());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello from the server" });
