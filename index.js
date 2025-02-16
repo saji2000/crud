69150a670280fb8ad9bd0127944e9712549e45d9
saji2000 <sajaddaneshmand79@gmail.com>
2025-02-16 15:31:14 -0500

    mongoose added, praise God

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const cookieParse = require("cookie-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(cors());
@@ -9,6 +10,11 @@ app.use(helmet());
 app.use(express.json());
 app.use(cookieParse());
 app.use(express.urlencoded({ extended: true }));
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Database Connected"))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello from the server" });
