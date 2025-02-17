dbc79beb18b6d255eb76a038eab582b60b2e0590
saji2000 <sajaddaneshmand79@gmail.com>
2025-02-17 14:27:30 -0500

    working on routers, praise God

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const cookieParse = require("cookie-parser");
 const mongoose = require("mongoose");
 
+const authRouter = require("./routers/authRouter");
+
 const app = express();
 app.use(cors());
 app.use(helmet());
@@ -15,6 +17,8 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Database Connected"))
   .catch((err) => console.log(err));
+
+app.use("/api/auth", authRouter);
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello from the server" });
---
+++ routers/authRouter.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.post("/signup", (req, res) => {
+  res.json({ message: "Sign up successful" });
+});
+
+module.exports = router;
