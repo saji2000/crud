000ecb4f39b07556a5bc3a082e6fee1c5578d97a
saji2000 <sajaddaneshmand79@gmail.com>
2025-02-26 20:10:33 -0500

    Adding the post apis, praise God

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,102 +1,28 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { signupSchema, signinSchema } = require("../middleware/validator");
-const User = require("../models/usersModel");
+const Posts = require("../models/postsModel");
 
 // Sign Up Function
-exports.signup = async (req, res) => {
-  const { email, password } = req.body;
+exports.getAll = async (req, res) => {
+  const { page } = req.query;
+  const postsPerPage = 10;
   try {
-    const { error, value } = signupSchema.validate({
-      email: email,
-      password: password,
-    });
-
-    if (error) {
-      return res
-        .status(401)
-        .json({ success: false, message: error.details[0].message });
+    let pageNum = 0;
+    if (page <= 1) {
+      pageNum = 0;
+    } else {
+      pageNum = page - 1;
     }
 
-    const existingUser = await User.findOne({ email: email });
+    const result = await Posts.find()
+      .sort({ createdAt: -1 })
+      .skip(pageNum * postsPerPage)
+      .limit(postsPerPage)
+      .populate({ path: "userId", select: "email" });
 
-    if (existingUser) {
-      return res
-        .status(401)
-        .json({ success: false, message: "User already exists" });
-    }
-
-    const hashedPassword = await hash(password);
-
-    const newUser = await new User({ email, password: hashedPassword });
-
-    const result = await newUser.save();
-
-    result.password = undefined;
-    res.status(201).json({
-      success: true,
-      message: "Your account has been created successfully.",
-      result,
-    });
+    res.status(200).json({ success: true, message: "posts", data: result });
   } catch (err) {
     console.log(err);
   }
-};
-
-// Sign In function
-exports.signin = async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    const { error } = signinSchema.validate({
-      email: email,
-    });
-
-    if (error) {
-      return res
-        .status(401)
-        .json({ success: false, message: error.details[0].message });
-    }
-
-    const user = await User.findOne({ email: email }).select("+password");
-
-    if (!user) {
-      return res
-        .status(400)
-        .json({ success: false, message: "Email does not exists" });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res
-        .status(400)
-        .json({ success: false, message: "Wrong password" });
-    }
-
-    const token = await jwt.sign(
-      { userId: user._id, email: user.email, verified: user.verified },
-      process.env.SECRET_KEY,
-      {
-        expiresIn: "12h",
-      }
-    );
-
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "Strict",
-      maxAge: 12 * 60 * 60 * 1000, // 12 hours in milliseconds
-    });
-
-    return res.status(200).json({ success: true, message: "Signed In" });
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-exports.signout = async (req, res) => {
-  res
-    .clearCookie("token")
-    .status(200)
-    .json({ success: true, message: "logged out successfully" });
 };
--- routers/postRouter.js
+++ routers/postRouter.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const postController = require("../controllers/authController");
+const postController = require("../controllers/postController");
 // Router for the APIs
 const router = express.Router();
 
 router.get("/all-posts", postController.getAll);
-router.get("/one-post", postController.getOne);
-router.post("/create-post", postController.create);
-router.put("/update-post", postController.update);
-router.delete("/delete-post", postController.delete);
+// router.get("/one-post", postController.getOne);
+// router.post("/create-post", postController.create);
+// router.put("/update-post", postController.update);
+// router.delete("/delete-post", postController.delete);
 // router.post("/verification", authController.sendVerificationCode);
 
 module.exports = router;
